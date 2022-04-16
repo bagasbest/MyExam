a1d57d8c9b0033659ef01444bef2e894c309fe69
bagasbest <45277197+bagasbest@users.noreply.github.com>
2022-04-16 14:43:11 +0700

    finish

--- app/src/main/java/com/project/myexam/MainActivity.kt
+++ app/src/main/java/com/project/myexam/MainActivity.kt
@@ -3,7 +3,9 @@ package com.project.myexam
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import com.project.myexam.databinding.ActivityMainBinding
 import com.project.myexam.homepage.HomepageFragment
@@ -18,31 +20,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        val navView = findViewById<ChipNavigationBar>(R.id.nav_view)
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        navView.setOnNavigationItemSelectedListener { item: MenuItem ->
+            var selectedFragment: Fragment? = HomepageFragment()
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    navView.menu.findItem(R.id.navigation_home).isEnabled = false
+                    navView.menu.findItem(R.id.navigation_maps).isEnabled = true
+                    selectedFragment = HomepageFragment()
+                }
 
-        navView.setItemSelected(R.id.navigation_home, true)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, HomepageFragment()).commit()
-
-
-        bottomMenu(navView)
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    private fun bottomMenu(navView: ChipNavigationBar) {
-        navView.setOnItemSelectedListener { i: Int ->
-            var fragment: Fragment? = null
-            when (i) {
-                R.id.navigation_home -> fragment = HomepageFragment()
-                R.id.navigation_maps -> fragment = MapsFragment()
+                R.id.navigation_maps -> {
+                    navView.menu.findItem(R.id.navigation_home).isEnabled = true
+                    navView.menu.findItem(R.id.navigation_maps).isEnabled = false
+                    selectedFragment = MapsFragment()
+                }
             }
-            supportFragmentManager.beginTransaction()
-                .replace(
-                    R.id.container,
-                    fragment!!
-                ).commit()
+            val transaction = supportFragmentManager.beginTransaction()
+            if (selectedFragment != null) {
+                transaction.replace(R.id.container, selectedFragment)
+            }
+            transaction.commit()
+            true
         }
     }
+
     override fun onDestroy() {
         super.onDestroy()
         binding = null
--- app/src/main/java/com/project/myexam/maps/MapsFragment.kt
+++ app/src/main/java/com/project/myexam/maps/MapsFragment.kt
@@ -3,22 +3,22 @@ package com.project.myexam.maps
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Color
-import android.graphics.Typeface
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
@@ -31,17 +31,17 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.tasks.OnCompleteListener
 import com.project.myexam.R
 import com.project.myexam.databinding.FragmentMapsBinding
 import com.project.myexam.utils.DataModel
 import com.project.myexam.utils.DataRepository
+import java.util.*
 
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
 
     private var binding: FragmentMapsBinding? = null
-    private var mMap: GoogleMap? = null
+    private lateinit var mMap: GoogleMap
     private lateinit var currentLocation: Location
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val permissionCode = 101
@@ -67,6 +67,50 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
 
         return binding?.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.searchBtn?.setOnClickListener {
+            validateForm()
+        }
+    }
+
+    private fun validateForm() {
+        val query = binding?.searchEt?.text.toString().trim().toLowerCase()
+
+        if(query.isEmpty()) {
+            Toast.makeText(activity, "Sorry, Location name must be filled", Toast.LENGTH_SHORT).show()
+        } else {
+            val progressDialog = ProgressDialog(activity)
+            progressDialog.setCancelable(false)
+            progressDialog.setMessage("Waiting until process finished")
+            progressDialog.show()
+
+
+            for(i in 0 until dataList.size) {
+
+                if(dataList[i].tempName!!.contains(query)) {
+                    progressDialog.dismiss()
+                    val latitude: Double = dataList[i].latitude
+                    val longitude: Double = dataList[i].longitude
+
+                    Log.e("tag", "$latitude|$longitude")
+
+
+                    val latLng = LatLng(latitude, longitude)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
+                    break
+                } else if(i == dataList.size-1) {
+                    progressDialog.dismiss()
+                    Toast.makeText(activity, "Sorry, Location not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
 
     private fun fetchLocation() {
         if (ActivityCompat.checkSelfPermission(
@@ -100,20 +144,23 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
 
     @SuppressLint("PotentialBehaviorOverride")
     override fun onMapReady(googleMap: GoogleMap) {
+
+        mMap = googleMap
+
         val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
         val markerOptions = MarkerOptions().position(latLng).title("Your Location")
             .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7f))
-        googleMap.addMarker(markerOptions)
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7f))
+        mMap.addMarker(markerOptions)
         if (ContextCompat.checkSelfPermission(
                 requireActivity(),
                 Manifest.permission.ACCESS_FINE_LOCATION
             )
             == PackageManager.PERMISSION_GRANTED
         ) {
-            googleMap.isMyLocationEnabled = true
-            googleMap.uiSettings.isMyLocationButtonEnabled = true
+            mMap.isMyLocationEnabled = true
+            mMap.uiSettings.isMyLocationButtonEnabled = true
         }
 
         for (i in 0 until dataList.size) {
@@ -121,33 +168,40 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
             val longitude: Double = dataList[i].longitude
             val name = "" + dataList[i].name
             val address = "" + dataList[i].address
+            val image = "" + dataList[i].image
 
             val latLng1 = LatLng(latitude, longitude)
-            googleMap.addMarker(
+            mMap.addMarker(
                 MarkerOptions().position(latLng1).title(name)
-                    .snippet(address)
+                    .snippet("$address|$image")
             )
         }
-        googleMap.setInfoWindowAdapter(object : InfoWindowAdapter {
+        mMap.setInfoWindowAdapter(object : InfoWindowAdapter {
             override fun getInfoWindow(marker: Marker): View? {
                 return null
             }
 
-            override fun getInfoContents(marker: Marker): View? {
-                val info = LinearLayout(context)
-                info.orientation = LinearLayout.VERTICAL
-                val title = TextView(context)
-                title.setTextColor(Color.BLACK)
-                title.gravity = Gravity.LEFT
-                title.setTypeface(null, Typeface.BOLD)
-                title.text = marker.title
+            override fun getInfoContents(marker: Marker): View {
+                @SuppressLint("InflateParams") val row: View = layoutInflater.inflate(R.layout.custom_snippet, null)
+                val img = row.findViewById<ImageView>(R.id.image)
+                val name = row.findViewById<TextView>(R.id.name)
+                val address = row.findViewById<TextView>(R.id.address)
 
-                val snippet = TextView(context)
-                snippet.setTextColor(Color.GRAY)
-                snippet.text = marker.snippet
-                info.addView(title)
-                info.addView(snippet)
-                return info
+                name.text = marker.title
+                if (!marker.title.equals("Your Location")) {
+                    img.visibility = View.VISIBLE
+                    address.visibility = View.VISIBLE
+                    Glide.with(requireActivity())
+                        .load(
+                            Objects.requireNonNull(marker.snippet)?.substring(marker.snippet!!.indexOf("|") + 1)
+                        )
+                        .into(img)
+                    address.text = marker.snippet!!.substring(0, marker.snippet!!.indexOf("|"))
+                } else {
+                    img.visibility = View.GONE
+                    address.visibility = View.GONE
+                }
+                return row
             }
         })
     }
